7dc6e8a05e074fa57857e2ad6c08767262a5aa69
VacaSan <kotaracvladan@gmail.com>
2017-10-20 18:39:47 +0200

    name changes

--- src/range/index.js
+++ src/range/index.js
@@ -1,11 +1,15 @@
 export default class {
-  constructor (id, max, min = 0) {
+  constructor (id, max) {
+    // range
     this._max = max;
-    this._min = min;
+
+    // cache DOM
     this.component = document.getElementById(id);
     this.track = this.component.querySelector('#js-range__track');
-    this.controlMin = this.component.querySelector('[data-controls="min"]');
-    this.controlMax = this.component.querySelector('[data-controls="max"]');
+    this.controls = {
+      min: this.component.querySelector('[data-controls="min"]'),
+      max: this.component.querySelector('[data-controls="max"]')
+    }
 
     this.onStart = this.onStart.bind(this);
     this.onMove = this.onMove.bind(this);
@@ -13,101 +17,72 @@ export default class {
     this.update = this.update.bind(this);
 
     this._gBCR = this.component.getBoundingClientRect();
-    this.componentX = this._gBCR.left;
-    this.width = this._gBCR.width;
+    this._state = {
+      min: 0,
+      max: this.toPx(this._max)
+    }
 
-    this.eventTarget = null;
-    this.currentX = 0;
+    this._eventTarget = null;
+    this._currentX = 0;
+    this._knob = '';
 
-    this.addEventListeners();
+    this._addEventListeners();
   }
 
-  addEventListeners () {
+  _addEventListeners () {
     document.addEventListener('mousedown', this.onStart);
     document.addEventListener('mousemove', this.onMove);
     document.addEventListener('mouseup', this.onEnd);
   }
 
-  _render () {
-    this.controlMin.style.transform = `translateX(${this.currentX}px) translate(-50%, -50%)`;
-    this.controlMax.style.transform = `translateX(${this.currentX}px) translate(-50%, -50%)`;
-  }
-
-  _valueToPx (val) {
-    return (val / this._max) * this.width; //px
-  }
-
-  get min () {
-    return this._min;
-  }
-
-  set min (val) {
-    // val: number
-    this._min = val;
-  }
-
-  get max () {
-    return this._max;
-  }
-
-  set max (val) {
-    // val: number
-    this._max = val;
-  }
-
-  get value () {
-    return this._value;
-  }
-
-  set value (val) {
-    // val: array = [0, n],  n > 0
-    this._value = {
-      min: Math.min(...val),
-      max: Math.max(...val)
-    }
-  }
-
   onStart (evt) {
-    if (this.eventTarget)
+    if (this._eventTarget)
       return;
 
     if (!evt.target.classList.contains('knob'))
       return;
 
-    this.eventTarget = evt.target;
-    this.componentX = this.component.offsetLeft;
-    this.currentX = evt.pageX - this.componentX;
+    this._eventTarget = evt.target;
+    this._knob = this._eventTarget.getAttribute('data-controls');
+    this._currentX = evt.pageX - this._gBCR.left;
     this.rAF = requestAnimationFrame(this.update);
 
-    this.eventTarget.classList.add('range__control--active');
+    this._eventTarget.classList.add('range__control--active');
   }
 
   onMove (evt) {
-    if (!this.eventTarget)
+    if (!this._eventTarget)
       return;
 
-    this.currentX = evt.pageX - this.componentX;
+    this._currentX = evt.pageX - this._gBCR.left;
   }
 
   onEnd (evt) {
-    if (!this.eventTarget)
+    if (!this._eventTarget)
       return;
 
-    this.eventTarget.classList.remove('range__control--active');
-    this.eventTarget = null;
+    this._eventTarget.classList.remove('range__control--active');
+    this._eventTarget = null;
   }
 
   update () {
     this.rAF = requestAnimationFrame(this.update);
 
-    if (!this.eventTarget)
+    if (!this._eventTarget)
       return;
 
-    if (this.currentX < this._min)
-      this.currentX = this._min;
-    else if (this.currentX > this._valueToPx(this._max))
-      this.currentX = this.width;
+    // Change rules for each knob
+    if (this._currentX < 0)
+      this._currentX = 0;
+    else if (this._currentX > this.toPx(this._max, this._max, this._gBCR.width))
+      this._currentX = this._gBCR.width;
 
-    this.eventTarget.style.transform = `translateX(${this.currentX}px) translate(-50%, -50%)`;
+    this._eventTarget.style.transform =
+      `translateX(${this._currentX}px) translate(-50%, -50%)`;
+  }
+
+  toPx (val) {
+    return (val / this._max) * this._gBCR.width; //px
   }
 }
+
