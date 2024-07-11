2330e9f39ee2ea76966a31643c010fea01c94164
vacasan <kotaracvladan@gmail.com>
2024-07-11 17:58:12 +0200

    chore: upgrade open-wc

---
+++ test/double-slider.test.ts
@@ -0,0 +1,8 @@
+import { expect } from "@open-wc/testing";
+import "../src/double-slider.js";
+
+describe("double-slider", () => {
+  it("runs tests", async () => {
+    expect(true).to.equal(true);
+  });
+});
